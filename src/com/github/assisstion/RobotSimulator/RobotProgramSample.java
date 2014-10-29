f6e605d72569fe5003ca69fa615398fce0cff8cf
Markus Feng <zzz1112222595@hotmail.com>
2014-10-28 21:13:45 -0400

    Added simple sample code for figure 8

--- src/com/github/assisstion/RobotSimulator/RobotProgramSample.java
+++ src/com/github/assisstion/RobotSimulator/RobotProgramSample.java
@@ -10,6 +10,23 @@ public class RobotProgramSample extends RobotProgram{
 
 	@Override
 	public void run(){
+
+		/*
+		 * Simple sample code for figure 8:
+		 * Given 2 motors 5 units apart,
+		 * Moving at (speed) units per second
+
+		while(true){
+			motor[motorA] = 100;
+			motor[motorB] = 95;
+			wait1Msec(6283);
+			motor[motorA] = 95;
+			motor[motorB] = 100;
+			wait1Msec(6283);
+		}
+
+		 */
+
 		float speed = 100;
 		while(true){
 			motor[motorA] = speed;
